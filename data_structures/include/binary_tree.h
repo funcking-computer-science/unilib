052a4baed7776dc79d8b2c3145ecb359545439c6
mejjete <dani1.mlp1@gmail.com>
2020-10-18 23:28:34 +0300

    (feat) added simple binary tree implementation

--- data_structures/include/binary_tree.h
+++ data_structures/include/binary_tree.h
@@ -1,5 +1,8 @@
 #ifndef UNILIB_BINARY_TREE_
     #define UNILIB_BINARY_TREE_
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
 /*
  *
@@ -7,12 +10,23 @@
  * 
 */
 
-struct binary_tree
+enum
 {
-    struct binary_tree *parent;
-    struct binary_tree *left;
-    struct binary_tree *right;
-    void *key;
+    B_TREE_EQUAL,
+    B_TREE_GREATER,
+    B_TREE_LESS
 };
+
+typedef int(*comparer)(void *, void *);
+struct bnode;
+
+struct bnode *create_bnode(void *k);
+
+/*
+ * comparer is a pointer to a function 
+ * which returns true if first argument 
+ * is greater then second, and  false otherwise
+*/
+void insert_btree(struct bnode *root, struct bnode *node, comparer comp);
 
 #endif
--- data_structures/source/binary_tree.c
+++ data_structures/source/binary_tree.c
@@ -1,1 +1,42 @@
 #include <binary_tree.h>
+
+struct bnode
+{
+    struct bnode *parent;
+    struct bnode *left;
+    struct bnode *right;
+    void *key;
+};
+
+struct bnode *create_bnode(void *k)
+{
+    if(k == NULL)
+        return NULL;
+    struct bnode *node = (struct bnode *) malloc(sizeof(struct bnode));
+    node->parent = node->left = node->right = NULL;
+    node->key = k;
+    return node;
+};
+
+
+void insert_btree(struct bnode *root, struct bnode *node, comparer comp)
+{  
+    //if tree is empty initialize it with node
+    if(root == NULL)
+    {
+        root = node;
+        return;
+    }
+
+    //otherwise search the appropriate place
+    if(comp(root->key, node->key) == B_TREE_GREATER)
+    {
+        printf("go to left tree with: [%d]\n", *((int *)(node->key)));
+        insert_btree(root->left, node, comp);
+    }
+    else 
+    {
+        printf("go to right tree with: [%d]\n", *((int *)(node->key)));
+        insert_btree(root->right, node, comp);
+    }
+}
